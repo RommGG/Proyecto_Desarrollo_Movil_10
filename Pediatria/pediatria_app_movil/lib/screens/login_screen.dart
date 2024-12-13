06d9870224d5ba3f4671867da7093eba2b2658ba
Romualdo <210714@utxicotepec.edu.mx>
2024-12-12 22:24:27 -0600

    cambios en los logos

--- Pediatria/pediatria_app_movil/lib/screens/login_screen.dart
+++ Pediatria/pediatria_app_movil/lib/screens/login_screen.dart
@@ -6,10 +6,79 @@ class LoginScreen extends StatelessWidget {
   final TextEditingController emailController = TextEditingController();
   final TextEditingController passwordController = TextEditingController();
 
+  // Método para iniciar sesión con GitHub
+  Future<void> signInWithGitHub(BuildContext context) async {
+    try {
+      // Define el proveedor de GitHub
+      final GithubAuthProvider githubProvider = GithubAuthProvider();
+
+      // Intenta iniciar sesión con GitHub
+      UserCredential userCredential =
+          await FirebaseAuth.instance.signInWithProvider(githubProvider);
+
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Inicio de sesión con GitHub exitoso.')),
+      );
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'account-exists-with-different-credential') {
+        // El correo ya existe con un proveedor diferente
+        String email = e.email!; // Correo asociado al error
+        AuthCredential? pendingCredential =
+            e.credential; // Credencial de GitHub
+
+        // Obtén los métodos de inicio de sesión asociados al correo
+        List<String> signInMethods =
+            await FirebaseAuth.instance.fetchSignInMethodsForEmail(email);
+
+        if (signInMethods.contains('google.com')) {
+          // Si el proveedor es Google, pide al usuario que inicie sesión con Google
+          final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
+          if (googleUser == null) {
+            ScaffoldMessenger.of(context).showSnackBar(
+              SnackBar(content: Text('Inicio de sesión con Google cancelado.')),
+            );
+            return;
+          }
+          final GoogleSignInAuthentication googleAuth =
+              await googleUser.authentication;
+
+          // Credencial de Google
+          final googleCredential = GoogleAuthProvider.credential(
+            accessToken: googleAuth.accessToken,
+            idToken: googleAuth.idToken,
+          );
+
+          // Inicia sesión con Google y vincula la credencial pendiente
+          UserCredential googleUserCredential = await FirebaseAuth.instance
+              .signInWithCredential(googleCredential);
+
+          await googleUserCredential.user!
+              .linkWithCredential(pendingCredential!);
+
+          ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(
+                content: Text(
+                    'Cuenta vinculada correctamente con GitHub y Google.')),
+          );
+        } else {
+          // Agrega soporte para otros proveedores si es necesario
+          ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(
+                content: Text(
+                    'El correo ya está asociado a otro proveedor: $signInMethods')),
+          );
+        }
+      } else {
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text('Error al iniciar sesión con GitHub: $e')),
+        );
+      }
+    }
+  }
+
   // Método para iniciar sesión con Google
   Future<void> signInWithGoogle(BuildContext context) async {
     try {
-      // Inicia sesión con Google
       final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
       if (googleUser == null) {
         ScaffoldMessenger.of(context).showSnackBar(
@@ -17,25 +86,19 @@ class LoginScreen extends StatelessWidget {
         );
         return;
       }
-      //obtiene las credenciales de autenticación del usuario que se ha autenticado a través de Google.
       final GoogleSignInAuthentication googleAuth =
-          await googleUser.authentication; //
-      //se crean las credenciales necesarias para Firebase 
-      //Authentication utilizando el proveedor de Google (GoogleAuthProvider).
+          await googleUser.authentication;
       final credential = GoogleAuthProvider.credential(
         accessToken: googleAuth.accessToken,
         idToken: googleAuth.idToken,
       );
-
-      // Usa el token de Google para iniciar sesión en Firebase
       await FirebaseAuth.instance.signInWithCredential(credential);
-      //Mensaje de aceso
       ScaffoldMessenger.of(context).showSnackBar(
         SnackBar(content: Text('Inicio de sesión con Google exitoso.')),
       );
     } catch (e) {
       ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(content: Text('Error al iniciar sesión con Google: $e')), //mensaje de error
+        SnackBar(content: Text('Error al iniciar sesión con Google: $e')),
       );
     }
   }
@@ -43,39 +106,34 @@ class LoginScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      // Fondo de la imagen
       body: Stack(
         children: [
-          // Imagen de fondo
           Positioned.fill(
             child: Image.asset(
-              'lib/assets/images/fondo_doc.jpg', // Asegúrate de tener esta imagen
+              'lib/assets/images/fondo_doc.jpg',
               fit: BoxFit.cover,
             ),
           ),
-          // Contenedor de los campos de login con fondo transparente
           Center(
             child: Container(
               width: 350,
               padding: EdgeInsets.all(20),
               decoration: BoxDecoration(
-                color: Colors.white.withOpacity(0.8), // Fondo blanco con transparencia
+                color: Colors.white.withOpacity(0.8),
                 borderRadius: BorderRadius.circular(15),
               ),
               child: Column(
                 mainAxisSize: MainAxisSize.min,
                 children: [
-                  // Título
                   Text(
                     'Iniciar sesión',
                     style: TextStyle(
                       fontSize: 24,
                       fontWeight: FontWeight.bold,
-                      color: const Color(0xFF0D47A1), // Color destacado
+                      color: const Color(0xFF0D47A1),
                     ),
                   ),
                   SizedBox(height: 20),
-                  // Campo de email
                   TextField(
                     controller: emailController,
                     decoration: InputDecoration(
@@ -90,7 +148,6 @@ class LoginScreen extends StatelessWidget {
                     ),
                   ),
                   SizedBox(height: 20),
-                  // Campo de contraseña
                   TextField(
                     controller: passwordController,
                     obscureText: true,
@@ -106,12 +163,10 @@ class LoginScreen extends StatelessWidget {
                     ),
                   ),
                   SizedBox(height: 20),
-                  // Botón de login con email
                   ElevatedButton(
                     onPressed: () async {
                       try {
-                        await FirebaseAuth.instance
-                            .signInWithEmailAndPassword(
+                        await FirebaseAuth.instance.signInWithEmailAndPassword(
                           email: emailController.text.trim(),
                           password: passwordController.text.trim(),
                         );
@@ -120,13 +175,15 @@ class LoginScreen extends StatelessWidget {
                         );
                       } catch (e) {
                         ScaffoldMessenger.of(context).showSnackBar(
-                          SnackBar(content: Text('Error al iniciar sesión: $e')),
+                          SnackBar(
+                              content: Text('Error al iniciar sesión: $e')),
                         );
                       }
                     },
                     child: Text('Login con Email'),
                     style: ElevatedButton.styleFrom(
-                      foregroundColor: Colors.white, backgroundColor: Colors.blue[900],
+                      foregroundColor: Colors.white,
+                      backgroundColor: Colors.blue[900],
                       minimumSize: Size(double.infinity, 50),
                       shape: RoundedRectangleBorder(
                         borderRadius: BorderRadius.circular(10),
@@ -134,13 +191,26 @@ class LoginScreen extends StatelessWidget {
                     ),
                   ),
                   SizedBox(height: 20),
-                  // Botón de login con Google
                   ElevatedButton.icon(
                     onPressed: () => signInWithGoogle(context),
-                    
                     label: Text('Login con Google'),
                     style: ElevatedButton.styleFrom(
-                      foregroundColor: Colors.white, backgroundColor: const Color(0xFFF44336),
+                      foregroundColor: Colors.white,
+                      backgroundColor: const Color(0xFFF44336),
+                      minimumSize: Size(double.infinity, 50),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 20),
+                  ElevatedButton.icon(
+                    onPressed: () => signInWithGitHub(context),
+                    icon: Icon(Icons.code),
+                    label: Text('Login con GitHub'),
+                    style: ElevatedButton.styleFrom(
+                      foregroundColor: Colors.white,
+                      backgroundColor: Colors.black,
                       minimumSize: Size(double.infinity, 50),
                       shape: RoundedRectangleBorder(
                         borderRadius: BorderRadius.circular(10),
